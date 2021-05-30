bffb28deaccbf3fccd23bab9a34c3f4cffe71583
IsaqueGarcia <isaqueemprego1@gmail.com>
2021-05-30 13:24:12 -0300

    Loading screen implemented for loaded scene components

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressTxt;
+    public List<Texture> rawTexture;
+    public RawImage rawImage;
+    public Text tipsText;
+    
+    public void LoadLevel(int sceneIndex)
+    {
+
+        StartCoroutine(LoadAsyncScene(sceneIndex));
+        StartCoroutine("ChangeTexture");
+        StartCoroutine("ChangeTips");
+    }
+
+
+    IEnumerator LoadAsyncScene(int sceneIndex)
+    {
+        //AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+
+        loadingScreen.SetActive(true);
+        /*
+         while(!operation.isDone)
+         {
+             float progress = Mathf.Clamp01(operation.progress / .9f);
+
+             slider.value = progress;
+             progressTxt.text = progress * 100f + "%";
+
+             yield return null;
+         }
+        */
+        yield return null;
+    }
+
+    IEnumerator ChangeTexture()
+    {
+        rawImage.texture = rawTexture[0];
+
+        yield return new WaitForSeconds(5f);
+
+        rawImage.texture = rawTexture[1];
+
+        yield return new WaitForSeconds(5f);
+
+        rawImage.texture = rawTexture[2];
+
+        yield return new WaitForSeconds(5f);
+
+        StartCoroutine("ChangeTexture");
+    }
+
+    IEnumerator ChangeTips()
+    {
+        tipsText.text = "Beware of golems, they have special attacks when reaching a certain amount of life.";
+
+        yield return new WaitForSeconds(7f);
+
+        tipsText.text = "Watch out for witches they just look weak.";
+
+        yield return new WaitForSeconds(7f);
+
+        tipsText.text = "You can buy equipment and potions by talking to the bald man in the middle of the village of aurora.";
+
+        yield return new WaitForSeconds(7f);
+
+        tipsText.text = "Each enemy has up to two attack variations, except the boss ...";
+
+        yield return new WaitForSeconds(7f);
+
+        tipsText.text = "Make your story, tell your adventures, around the bonfire, this is the purpose of the game.";
+
+        yield return new WaitForSeconds(7f);
+
+        StartCoroutine("ChangeTips");
+
+    }
+
+}
--- Assets/Project/Jonas/Scripts/GameGeneral/MenuController.cs
+++ Assets/Project/Jonas/Scripts/GameGeneral/MenuController.cs
@@ -13,6 +13,14 @@ public class MenuController : MonoBehaviour
     public Text createLobbyCode;
     public Text joinLobbyCode;
 
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressTxt;
+    public List<Texture> rawTexture;
+    public RawImage rawImage;
+    public Text tipsText;
+
+
     private void Awake()
     {
         PhotonNetwork.ConnectUsingSettings(VersionName);
@@ -43,6 +51,7 @@ public class MenuController : MonoBehaviour
         {
             JoinGame();
         }
+
     }
 
     public void CreateGame()
@@ -62,6 +71,71 @@ public class MenuController : MonoBehaviour
 
     private void OnJoinedRoom()
     {
-            PhotonNetwork.LoadLevel("Map");
+        StartCoroutine(LoadAsyncScene());
+    }
+
+
+    IEnumerator LoadAsyncScene()
+    {
+
+        AsyncOperation operation = PhotonNetwork.LoadLevelAsync("Map");
+
+        loadingScreen.SetActive(true);
+        StartCoroutine("ChangeTexture");
+        StartCoroutine("ChangeTips");
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+            progressTxt.text = progress * 100f + "%";
+
+            yield return null;
+        }
+
+    }
+
+    IEnumerator ChangeTexture()
+    {
+        rawImage.texture = rawTexture[0];
+
+        yield return new WaitForSeconds(5f);
+
+        rawImage.texture = rawTexture[1];
+
+        yield return new WaitForSeconds(5f);
+
+        rawImage.texture = rawTexture[2];
+
+        yield return new WaitForSeconds(5f);
+
+        StartCoroutine("ChangeTexture");
+    }
+
+    IEnumerator ChangeTips()
+    {
+        tipsText.text = "Beware of golems, they have special attacks when reaching a certain amount of life.";
+
+        yield return new WaitForSeconds(7f);
+
+        tipsText.text = "Watch out for witches they just look weak.";
+
+        yield return new WaitForSeconds(7f);
+
+        tipsText.text = "You can buy equipment and potions by talking to the bald man in the middle of the village of aurora.";
+
+        yield return new WaitForSeconds(7f);
+
+        tipsText.text = "Each enemy has up to two attack variations, except the boss ...";
+
+        yield return new WaitForSeconds(7f);
+
+        tipsText.text = "Make your story, tell your adventures, around the bonfire, this is the purpose of the game.";
+
+        yield return new WaitForSeconds(7f);
+
+        StartCoroutine("ChangeTips");
+
     }
 }
